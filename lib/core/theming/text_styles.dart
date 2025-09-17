e7c6f8826365ba5851de8cea8681019c99ca474c
MaiBomadian94 <Maibomadian@gmail.com>
2025-09-17 18:03:40 +0300

    * create app text styles

---
+++ lib/core/theming/text_styles.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class TextStyles {
+  static const font = TextStyle(
+      fontWeight: FontWeight.w400,
+      fontSize: 13,
+      fontFamily: 'IBMPlexSansArabic'
+
+  );
+  static const textTitle11 = TextStyle(
+      fontWeight: FontWeight.w400,
+      fontSize: 11,
+      fontFamily: 'IBMPlexSansArabic'
+
+  );
+  static const textTitle12 = TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 12,
+      fontFamily: 'IBMPlexSansArabic'
+
+  );
+
+  static const textTitle14 = TextStyle(
+      fontWeight: FontWeight.w400,
+      fontSize: 14,
+      fontFamily: 'IBMPlexSansArabic'
+
+  );
+  static const textTitle15 = TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 15,
+      fontFamily: 'IBMPlexSansArabic'
+  );
+  static const textTitle16 = TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      fontFamily: 'IBMPlexSansArabic'
+
+  );
+
+  static const textTitle18 = TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 18,
+      fontFamily: 'IBMPlexSansArabic'
+
+  );
+}
