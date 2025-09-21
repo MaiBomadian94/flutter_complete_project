d65d0649bf3911bbc015ecf70ed44cfa98b6b3a1
MaiiSmart <mai@smarttechno.co>
2025-09-21 22:23:30 +0300

    * create docdoc row

--- lib/core/theming/text_styles.dart
+++ lib/core/theming/text_styles.dart
@@ -1,47 +1,36 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 abstract class TextStyles {
-  static const font = TextStyle(
-      fontWeight: FontWeight.w400,
-      fontSize: 13,
-      fontFamily: 'IBMPlexSansArabic'
-
+  static const textTitle24 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+    fontFamily: 'IBMPlexSansArabic',
+    color: Colors.black,
   );
   static const textTitle11 = TextStyle(
       fontWeight: FontWeight.w400,
       fontSize: 11,
-      fontFamily: 'IBMPlexSansArabic'
-
-  );
+      fontFamily: 'IBMPlexSansArabic');
   static const textTitle12 = TextStyle(
       fontWeight: FontWeight.w500,
       fontSize: 12,
-      fontFamily: 'IBMPlexSansArabic'
-
-  );
+      fontFamily: 'IBMPlexSansArabic');
 
   static const textTitle14 = TextStyle(
       fontWeight: FontWeight.w400,
       fontSize: 14,
-      fontFamily: 'IBMPlexSansArabic'
-
-  );
+      fontFamily: 'IBMPlexSansArabic');
   static const textTitle15 = TextStyle(
       fontWeight: FontWeight.w500,
       fontSize: 15,
-      fontFamily: 'IBMPlexSansArabic'
-  );
+      fontFamily: 'IBMPlexSansArabic');
   static const textTitle16 = TextStyle(
       fontWeight: FontWeight.w600,
       fontSize: 16,
-      fontFamily: 'IBMPlexSansArabic'
-
-  );
+      fontFamily: 'IBMPlexSansArabic');
 
   static const textTitle18 = TextStyle(
       fontWeight: FontWeight.w500,
       fontSize: 18,
-      fontFamily: 'IBMPlexSansArabic'
-
-  );
+      fontFamily: 'IBMPlexSansArabic');
 }
--- lib/features/on_boarding/presentation/view/on_boarding_view.dart
+++ lib/features/on_boarding/presentation/view/on_boarding_view.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../core/theming/text_styles.dart';
 
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.only(top: 30.h),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset('assets/svgs/docdoc.svg'),
+                SizedBox(
+                  width: 10.w,
+                ),
+                const Text(
+                  'DocDoc',
+                  style: TextStyles.textTitle24,
+                )
+              ],
+            ),
+            SizedBox(height: 40.h,),
+          ],
+        ),
+      )),
+    );
   }
 }
