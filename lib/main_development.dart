7151c2f25c0f08f4a6879c30c0e0bd81e3462a85
MaiBomadian94 <Maibomadian@gmail.com>
2025-12-18 00:27:10 +0200

    * added Development & Production flavors for android * added main file for each flavor * change android app name for each flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/di/dependency_injection.dart';
 import 'doc_app.dart';
 
-void main() {
+void main() async{
+
+  await ScreenUtil.ensureScreenSize();
+
   setUpInjection();
   runApp(const DocApp());
 }
